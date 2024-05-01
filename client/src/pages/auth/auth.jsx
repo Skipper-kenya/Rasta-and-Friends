b51b7c4739f0b531163be47c39ecc5dd077d8c45
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-05-01 21:13:00 +0300

    1.0.0

--- client/src/pages/auth/auth.jsx
+++ client/src/pages/auth/auth.jsx
@@ -7,7 +7,14 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser, updateDetails } from "../../redux/user";
 import loading, { setLoading, hideLoading } from "../../redux/loading";
-import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { purgeContext } from "../../context/purge";
 import Footer from "../../components/Footer";
 
@@ -84,122 +91,136 @@ const Auth = ({ name, setStorage }) => {
   };
 
   return (
-    <>
-      <div className="auth_holder">
-        {viewportWidth <= 767 && <div className="left_wrapper"></div>}
+    <div className="auth_holder">
+      {viewportWidth <= 767 && <div className="left_wrapper"></div>}
 
-        {viewportWidth > 767 && (
-          <Box
-            backgroundColor="blue"
-            height="100vh"
-            zIndex={1000}
-            width="50%"
-            color="white"
-            display="flex"
-            alignItems="center"
-            flexDirection="column"
-          >
-            <Container>
-              <Typography variant="h2">
-                Rasta & Friends Hub: Everything you need to progress as a
-                developer.
+      {viewportWidth > 767 && (
+        <Box
+          backgroundColor="blue"
+          height="100vh"
+          zIndex={1000}
+          width="50%"
+          color="white"
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Container>
+            <Typography variant="h2">
+              Rasta & Friends Hub: Everything you need to progress as a
+              developer.
+            </Typography>
+            <Stack direction="column" spacing={1}>
+              <Divider />
+              <Typography>
+                This is a Developer's community. at rastaTech we believe in
+                collaboration and our website gives you room to engage with
+                like-minded developers like you.
               </Typography>
-              <Stack direction="column" spacing={1}>
-                <Divider />
-                <Typography>
-                  This is a Developer's community. at rastaTech we believe in
-                  collaboration and our website gives you room to engage with
-                  like-minded developers like you.
-                </Typography>
-                <Typography>
-                  You can showcase your projects and learn from other's
-                  projects. Additionally, the contributions page will allow you
-                  to raise any issue, concern or ask for help from your peers
-                </Typography>
-              </Stack>
-            </Container>
-          </Box>
-        )}
-
-        <Box>
-          <div
-            className="cardHolder"
-            style={{
-              minHeight: viewportWidth <= 767 ? "50vh" : " 500px",
-              paddingTop: viewportWidth <= 767 ? "2rem" : "4rem",
-              marginRight: viewportWidth <= 767 ? "8rem" : "4rem",
-            }}
-          >
-            <Card
-              className="card"
-              title={name.toUpperCase()}
-              style={{ width: "300px" }}
-              bordered
-            >
-              <Space direction="vertical">
-                <section>
-                  <label htmlFor="username">Username</label>
-                  <Input
-                    allowClear
-                    type="text"
-                    placeholder="username"
-                    size="large"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </section>
-                <section>
-                  <label htmlFor="username">Password</label>
-                  <Input.Password
-                    type="password"
-                    placeholder="password"
-                    size="large"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </section>
-                <section>
-                  <Button
-                    className="btn"
-                    type="primary"
-                    block
-                    size="large"
-                    onClick={handleSubmit}
-                    loading={loading}
-                  >
-                    {isRegisterRequest()
-                      ? "Sign up"
-                      : isLoginRequest()
-                      ? "Sign in"
-                      : ""}
-                  </Button>
-                </section>
-                <section>
-                  {isLoginRequest() ? (
-                    <>
-                      <span>
-                        Don't have an account?
-                        <a onClick={() => navigate("/register")}>Register</a>
-                      </span>
-                    </>
-                  ) : isRegisterRequest() ? (
-                    <>
-                      <span>
-                        have an account?{" "}
-                        <a onClick={() => navigate("/login")}>Login</a>
-                      </span>
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </section>
-              </Space>
-            </Card>
-          </div>
+              <Typography>
+                You can showcase your projects and learn from other's projects.
+                Additionally, the contributions page will allow you to raise any
+                issue, concern or ask for help from your peers
+              </Typography>
+            </Stack>
+          </Container>
         </Box>
-      </div>
-      <Footer />
-    </>
+      )}
+      <Toolbar>
+        <Stack
+          direction="column"
+          alignItems="center"
+          spacing={1}
+          sx={{ justifyContent: "center", width: "100%" }}
+        >
+          <Typography variant="h6" sx={{ color: "white", textAlign: "center" }}>
+            Welcome to Rasta & Friends
+          </Typography>
+          <Typography
+            sx={{ color: "white", textAlign: "center", fontStyle: "italic" }}
+          >
+            join this transformative developer's community today
+          </Typography>
+        </Stack>
+      </Toolbar>
+
+      <Box>
+        <div
+          className="cardHolder"
+          style={{
+            minHeight: viewportWidth <= 767 ? "50vh" : " 500px",
+            paddingTop: viewportWidth <= 767 ? "2rem" : "4rem",
+            marginRight: viewportWidth <= 767 ? "8rem" : "4rem",
+          }}
+        >
+          <Card
+            className="card"
+            title={name.toUpperCase()}
+            style={{ width: "300px" }}
+            bordered
+          >
+            <Space direction="vertical">
+              <section>
+                <label htmlFor="username">Username</label>
+                <Input
+                  allowClear
+                  type="text"
+                  placeholder="username"
+                  size="large"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </section>
+              <section>
+                <label htmlFor="username">Password</label>
+                <Input.Password
+                  type="password"
+                  placeholder="password"
+                  size="large"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </section>
+              <section>
+                <Button
+                  className="btn"
+                  type="primary"
+                  block
+                  size="large"
+                  onClick={handleSubmit}
+                  loading={loading}
+                >
+                  {isRegisterRequest()
+                    ? "Sign up"
+                    : isLoginRequest()
+                    ? "Sign in"
+                    : ""}
+                </Button>
+              </section>
+              <section>
+                {isLoginRequest() ? (
+                  <>
+                    <span>
+                      Don't have an account?
+                      <a onClick={() => navigate("/register")}>Register</a>
+                    </span>
+                  </>
+                ) : isRegisterRequest() ? (
+                  <>
+                    <span>
+                      have an account?{" "}
+                      <a onClick={() => navigate("/login")}>Login</a>
+                    </span>
+                  </>
+                ) : (
+                  ""
+                )}
+              </section>
+            </Space>
+          </Card>
+        </div>
+      </Box>
+    </div>
   );
 };
 export default Auth;
