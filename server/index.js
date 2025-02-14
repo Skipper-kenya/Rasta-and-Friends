81198e9c509d5428f1495ff28a05849063781b2b
Andrew Kimwetich <kimwetichandrew@gmail.com>
2025-02-14 23:55:16 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const cors = require("cors");
+
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 //local
